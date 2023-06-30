dfbb7f02c5c43596938122d10dfa81f4c29baa63
alexander melian <alexandermelian@outlook.com.ar>
2023-06-30 01:42:44 -0300

    add compose and refator database connection

--- database/setup.go
+++ database/setup.go
@@ -3,25 +3,51 @@ package database
 import (
 	"localhost/models"
 	"os"
+	"strings"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+const maxRetries = 5
+const retryDelay = 5 * time.Second
+
 var DB *gorm.DB
+
+func buildConnectionString(user, password, host, dbName string) string {
+	var connectionString strings.Builder
+
+	connectionString.WriteString(user)
+	connectionString.WriteByte(':')
+	connectionString.WriteString(password)
+	connectionString.WriteString("@tcp(")
+	connectionString.WriteString(host)
+	connectionString.WriteString(")/")
+	connectionString.WriteString(dbName)
+	connectionString.WriteString("?charset=utf8&parseTime=True&loc=Local")
+
+	return connectionString.String()
+}
 
 func Connect() error {
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
-	dbOptions := os.Getenv("DB_OPTIONS")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?" + dbOptions
+	var err error
+	var database *gorm.DB
+	for i := 0; i < maxRetries; i++ {
+		connectionString := buildConnectionString(dbUsername, dbPassword, dbHost, dbName)
+		database, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 
-	//dsn := "root:pass@tcp(127.0.0.1:3306)/golang?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+
+		time.Sleep(retryDelay)
+	}
 
 	if err != nil {
 		return err
